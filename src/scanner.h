3e81ca2beb7fadb3b9dbea2d3ad1be681ad62568
Denys Pylypenko <xpylypd00@stud.fit.vutbr.cz>
2024-09-29 19:06:00 +0200

    Added project structure + starting with scanner

---
+++ src/scanner.h
@@ -0,0 +1,16 @@
+/**
+ * @file scanner.h
+ * @author Denys Pylypenko (xpylypd00)
+ * @brief
+ * 
+ * @date 2024-09-29
+ * 
+ */
+
+
+#ifndef _SCANNER_H_
+#define _SCANNER_H_
+
+
+
+#endif // _SCANNER_H_
