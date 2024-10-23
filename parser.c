c22f6ed37ab1930dae700adfaff8b27da2d910c3
Mariia <xsydorm00@vutbr.cz>
2024-10-23 18:19:37 +0200

    improve error message

--- parser.c
+++ parser.c
@@ -24,8 +24,16 @@ enum ErrorCodes {
 #include "parser.h"
 #include "scanner.h"
 
-void unexpected_token() {
-    fprintf(stderr, "Syntax error: unexpected token\n");
+Token CurrentToken;
+
+Token getCurrentToken() {
+    Token token = getNextToken();
+    CurrentToken = token;
+    return token;
+}
+
+void unexpected_token(Token token) {
+    fprintf(stderr, "Syntax error: unexpected token '%s' in line %lu\n", token.data, token.line);
     exit(ERROR_SYNTAX);
 }
 
@@ -34,7 +42,7 @@ void parse() {
         fprintf(stderr, "Syntax error: invalid prologue\n");
         exit(ERROR_SYNTAX);
     }
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     while (token.type != T_EOF) {
         Token new_token = code(token);
         token = new_token;
@@ -43,18 +51,18 @@ void parse() {
 
 int parse_prolog() {
     int return_code = 0;
-    if (getNextToken().type != T_CONST ||
-        getNextToken().type != T_IFJ || 
-        getNextToken().type != T_ASSIGN ||
-        getNextToken().type != T_IMPORT ||
-        getNextToken().type != T_OPEN_PARENTHESES) {
+    if (getCurrentToken().type != T_CONST ||
+        getCurrentToken().type != T_IFJ || 
+        getCurrentToken().type != T_ASSIGN ||
+        getCurrentToken().type != T_IMPORT ||
+        getCurrentToken().type != T_OPEN_PARENTHESES) {
             return_code = 1;
     }
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type != T_STRING_TYPE || (strcmp(token.data, "ifj24.zig") != 0)) {
         return_code = 1;
     }
-    if (getNextToken().type != T_CLOSE_PARENTHESES || getNextToken().type != T_SEMICOLON) {
+    if (getCurrentToken().type != T_CLOSE_PARENTHESES || getCurrentToken().type != T_SEMICOLON) {
         return_code = 1;
     }
 
@@ -95,32 +103,32 @@ Token code(Token token) {
 
 // Funkce pro parsování IF konstrukce
 Token parse_if() {
-    if (getNextToken().type != T_OPEN_PARENTHESES) {
-        unexpected_token();
+    if (getCurrentToken().type != T_OPEN_PARENTHESES) {
+        unexpected_token(CurrentToken);
     }
     expression();  // Parsování výrazu uvnitř závorek
-    if (getNextToken().type != T_CLOSE_PARENTHESES) {
-        unexpected_token();
+    if (getCurrentToken().type != T_CLOSE_PARENTHESES) {
+        unexpected_token(CurrentToken);
     }
-    if (getNextToken().type != T_OPEN_BRACKET) {
-        unexpected_token();
+    if (getCurrentToken().type != T_OPEN_BRACKET) {
+        unexpected_token(CurrentToken);
     }
-    code(getNextToken());  // Parsování těla if-bloku
-    if (getNextToken().type != T_CLOSE_BRACKET) {
-        unexpected_token();
+    code(getCurrentToken());  // Parsování těla if-bloku
+    if (getCurrentToken().type != T_CLOSE_BRACKET) {
+        unexpected_token(CurrentToken);
     }
 
     // Zpracování volitelného else
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type == T_ELSE) {
-        if (getNextToken().type != T_OPEN_BRACKET) {
-            unexpected_token();
+        if (getCurrentToken().type != T_OPEN_BRACKET) {
+            unexpected_token(CurrentToken);
         }
-        code(getNextToken());  // Parsování else-bloku
-        if (getNextToken().type != T_CLOSE_BRACKET) {
-            unexpected_token();
+        code(getCurrentToken());  // Parsování else-bloku
+        if (getCurrentToken().type != T_CLOSE_BRACKET) {
+            unexpected_token(CurrentToken);
         }
-        return getNextToken();  // Vrátíme token pro další zpracování
+        return getCurrentToken();  // Vrátíme token pro další zpracování
     } else {
         return token;  // Vrátíme token pro další zpracování
     }
@@ -128,89 +136,89 @@ Token parse_if() {
 
 // Funkce pro parsování WHILE konstrukce
 Token parse_while() {
-    if (getNextToken().type != T_OPEN_PARENTHESES) {
-       unexpected_token();
+    if (getCurrentToken().type != T_OPEN_PARENTHESES) {
+       unexpected_token(CurrentToken);
     }
     //expression();  
-    if (getNextToken().type != T_CLOSE_PARENTHESES) {
-        unexpected_token();
+    if (getCurrentToken().type != T_CLOSE_PARENTHESES) {
+        unexpected_token(CurrentToken);
     }
-    if (getNextToken().type != T_OPEN_BRACKET) {
-        unexpected_token();
+    if (getCurrentToken().type != T_OPEN_BRACKET) {
+        unexpected_token(CurrentToken);
     }
-    code(getNextToken());  // Parsování těla while smyčky
-    if (getNextToken().type != T_CLOSE_BRACKET) {
-        unexpected_token(); 
+    code(getCurrentToken());  // Parsování těla while smyčky
+    if (getCurrentToken().type != T_CLOSE_BRACKET) {
+        unexpected_token(CurrentToken); 
     }
 }
 
 Token parse_variable_definition() {
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type != T_ID) {
-        unexpected_token();
+        unexpected_token(CurrentToken);
     }
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type != T_COLON && token.type != T_ASSIGN) {
-        unexpected_token();
+        unexpected_token(CurrentToken);
     }
     if (token.type == T_COLON) {
-        if (getNextToken().type != T_I32_ID || 
-        getNextToken().type != T_F64_ID || 
-        getNextToken().type != T_U8_ID ||
-        getNextToken().type != T_I32_NULLABLE ||
-        getNextToken().type != T_F64_NULLABLE ||
-        getNextToken().type != T_U8_NULLABLE) {
-        unexpected_token();
+        if (getCurrentToken().type != T_I32_ID || 
+        getCurrentToken().type != T_F64_ID || 
+        getCurrentToken().type != T_U8_ID ||
+        getCurrentToken().type != T_I32_NULLABLE ||
+        getCurrentToken().type != T_F64_NULLABLE ||
+        getCurrentToken().type != T_U8_NULLABLE) {
+        unexpected_token(CurrentToken);
     }
-        if (getNextToken().type != T_ASSIGN) {
-        unexpected_token();
+        if (getCurrentToken().type != T_ASSIGN) {
+        unexpected_token(CurrentToken);
     }
     }
     expression();  // Parsování výrazu na pravé straně přiřazení
-    if (getNextToken().type != T_SEMICOLON) {
-        unexpected_token();
+    if (getCurrentToken().type != T_SEMICOLON) {
+        unexpected_token(CurrentToken);
     }
-    return getNextToken();  // Vrátíme token pro další zpracování
+    return getCurrentToken();  // Vrátíme token pro další zpracování
 }
 
 Token parse_return() {
     expression();  // Parsování výrazu, který se má vrátit
-    if (getNextToken().type != T_SEMICOLON) {
-        unexpected_token();
+    if (getCurrentToken().type != T_SEMICOLON) {
+        unexpected_token(CurrentToken);
     }
-    return getNextToken();  // Vrátíme token pro další zpracování
+    return getCurrentToken();  // Vrátíme token pro další zpracování
 }
 
 Token parse_assignment_or_function_call() {
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type != T_ASSIGN && token.type != T_OPEN_PARENTHESES) {
-        unexpected_token();
+        unexpected_token(CurrentToken);
     }
     if (token.type == T_ASSIGN) {
         expression();  // Parsování výrazu na pravé straně přiřazení
-        if (getNextToken().type != T_SEMICOLON) {
-            unexpected_token();
+        if (getCurrentToken().type != T_SEMICOLON) {
+            unexpected_token(CurrentToken);
         }
     } else {
         // Parsování volání funkce
         function();
-        if (getNextToken().type != T_CLOSE_PARENTHESES) {
-            unexpected_token();
+        if (getCurrentToken().type != T_CLOSE_PARENTHESES) {
+            unexpected_token(CurrentToken);
         }
-        if (getNextToken().type != T_SEMICOLON) {
-            unexpected_token();
+        if (getCurrentToken().type != T_SEMICOLON) {
+            unexpected_token(CurrentToken);
         }
     }
-    return getNextToken();  // Vrátíme token pro další zpracování
+    return getCurrentToken();  // Vrátíme token pro další zpracování
 }
 
 Token parse_standard_function_call() {
-    if (getNextToken().type != T_DOT) {
-        unexpected_token();
+    if (getCurrentToken().type != T_DOT) {
+        unexpected_token(CurrentToken);
     }
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type != T_ID) {
-        unexpected_token();
+        unexpected_token(CurrentToken);
     }
     if (!strcmp(token.data, "readstr") &&
         !strcmp(token.data, "readi32") &&
@@ -225,7 +233,7 @@ Token parse_standard_function_call() {
         !strcmp(token.data, "strcmp") &&
         !strcmp(token.data, "ord") &&
         !strcmp(token.data, "chr")) {
-        unexpected_token();
+        unexpected_token(CurrentToken);
     }
     if (!strcmp(token.data, "write") ||
         !strcmp(token.data, "i2f") ||
@@ -234,47 +242,47 @@ Token parse_standard_function_call() {
         !strcmp(token.data, "length") ||
         !strcmp(token.data, "ord") ||
         !strcmp(token.data, "chr")) {
-        if (getNextToken().type != T_OPEN_PARENTHESES) {
-            unexpected_token();
+        if (getCurrentToken().type != T_OPEN_PARENTHESES) {
+            unexpected_token(CurrentToken);
         }
         expression();  // Parsování argumentu funkce
-        if (getNextToken().type != T_CLOSE_PARENTHESES) {
-            unexpected_token();
+        if (getCurrentToken().type != T_CLOSE_PARENTHESES) {
+            unexpected_token(CurrentToken);
         }
     } else if (!strcmp(token.data, "concat") || !strcmp(token.data, "substrin") || !strcmp(token.data, "strcmp")) {
-        if (getNextToken().type != T_OPEN_PARENTHESES) {
-            unexpected_token();
+        if (getCurrentToken().type != T_OPEN_PARENTHESES) {
+            unexpected_token(CurrentToken);
         }
         expression();  // Parsování prvního argumentu funkce
-        if (getNextToken().type != T_COMMA) {
-            unexpected_token();
+        if (getCurrentToken().type != T_COMMA) {
+            unexpected_token(CurrentToken);
         }
         expression();  // Parsování druhého argumentu funkce
-        if (getNextToken().type != T_CLOSE_PARENTHESES) {
-            unexpected_token();
+        if (getCurrentToken().type != T_CLOSE_PARENTHESES) {
+            unexpected_token(CurrentToken);
         }
         // add optional comma?
     }
     else {
-        if (getNextToken().type != T_OPEN_PARENTHESES) {
-            unexpected_token();
+        if (getCurrentToken().type != T_OPEN_PARENTHESES) {
+            unexpected_token(CurrentToken);
         }
-        if (getNextToken().type != T_CLOSE_PARENTHESES) {
-            unexpected_token();
+        if (getCurrentToken().type != T_CLOSE_PARENTHESES) {
+            unexpected_token(CurrentToken);
         }
     }
-    if (getNextToken().type != T_SEMICOLON) {
-        unexpected_token();
+    if (getCurrentToken().type != T_SEMICOLON) {
+        unexpected_token(CurrentToken);
     }
-    return getNextToken();  // Vrátíme token pro další zpracování
+    return getCurrentToken();  // Vrátíme token pro další zpracování
 }
 
 void arguments(Token token) {
-    if (getNextToken().type != T_ID) {
-        unexpected_token();
+    if (getCurrentToken().type != T_ID) {
+        unexpected_token(CurrentToken);
     }
-    if (getNextToken().type != T_COLON) {
-        unexpected_token();
+    if (getCurrentToken().type != T_COLON) {
+        unexpected_token(CurrentToken);
     }
     if (token.type != T_I32_ID && 
         token.type != T_F64_ID && 
@@ -282,21 +290,21 @@ void arguments(Token token) {
         token.type != T_I32_NULLABLE &&
         token.type != T_F64_NULLABLE &&
         token.type != T_U8_NULLABLE) {
-        unexpected_token();
+        unexpected_token(CurrentToken);
     }
-    while (getNextToken().type != T_CLOSE_PARENTHESES) {
-        if (getNextToken().type != T_COMMA) {
-            unexpected_token();
+    while (getCurrentToken().type != T_CLOSE_PARENTHESES) {
+        if (getCurrentToken().type != T_COMMA) {
+            unexpected_token(CurrentToken);
         }
-        Token token = getNextToken();
+        Token token = getCurrentToken();
         if (token.type == T_CLOSE_PARENTHESES) {
             return;
         }
         else if (token.type != T_ID) {
-            unexpected_token();
+            unexpected_token(CurrentToken);
         }
-        if (getNextToken().type != T_COLON) {
-            unexpected_token();
+        if (getCurrentToken().type != T_COLON) {
+            unexpected_token(CurrentToken);
         }
         if (token.type != T_I32_ID && 
             token.type != T_F64_ID && 
@@ -304,42 +312,42 @@ void arguments(Token token) {
             token.type != T_I32_NULLABLE &&
             token.type != T_F64_NULLABLE &&
             token.type != T_U8_NULLABLE) {
-            unexpected_token();
+            unexpected_token(CurrentToken);
         }
     }
 }
 
 Token parse_function_definition() {
-    if (getNextToken().type != T_FN) {
-        unexpected_token();
+    if (getCurrentToken().type != T_FN) {
+        unexpected_token(CurrentToken);
     }
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type != T_ID) {
-        unexpected_token();
+        unexpected_token(CurrentToken);
     }
-    if (getNextToken().type != T_OPEN_PARENTHESES) {
-        unexpected_token();
+    if (getCurrentToken().type != T_OPEN_PARENTHESES) {
+        unexpected_token(CurrentToken);
     }
-    Token token = getNextToken();
+    Token token = getCurrentToken();
     if (token.type != T_CLOSE_PARENTHESES) {
         arguments(token);  // Parsování argumentů funkce
     }
-    if (getNextToken().type != T_I32_ID || 
-        getNextToken().type != T_F64_ID || 
-        getNextToken().type != T_U8_ID ||
-        getNextToken().type != T_I32_NULLABLE ||
-        getNextToken().type != T_F64_NULLABLE ||
-        getNextToken().type != T_U8_NULLABLE) {
-        unexpected_token();
+    if (getCurrentToken().type != T_I32_ID || 
+        getCurrentToken().type != T_F64_ID || 
+        getCurrentToken().type != T_U8_ID ||
+        getCurrentToken().type != T_I32_NULLABLE ||
+        getCurrentToken().type != T_F64_NULLABLE ||
+        getCurrentToken().type != T_U8_NULLABLE) {
+        unexpected_token(CurrentToken);
     }
-    if (getNextToken().type != T_OPEN_BRACKET) {
-        unexpected_token();
+    if (getCurrentToken().type != T_OPEN_BRACKET) {
+        unexpected_token(CurrentToken);
     }
-    code(getNextToken());  // Parsování těla funkce
-    if (getNextToken().type != T_CLOSE_BRACKET) {
-        unexpected_token();
+    code(getCurrentToken());  // Parsování těla funkce
+    if (getCurrentToken().type != T_CLOSE_BRACKET) {
+        unexpected_token(CurrentToken);
     }
-    return getNextToken();  // Vrátíme token pro další zpracování
+    return getCurrentToken();  // Vrátíme token pro další zpracování
 }
 
 // Hlavní program
