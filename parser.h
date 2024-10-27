9e845e43d739a566f58c569e6035b1d6c358a2d0
Mariia <xsydorm00@vutbr.cz>
2024-10-27 18:50:07 +0100

    define parser functions in header file

--- parser.h
+++ parser.h
@@ -9,26 +9,18 @@
 
 #include "scanner.h"
 
-/**
- * @brief struktura pro reprezentaci AST uzlu
- * 
- */
-typedef struct{
-    tASTNode* parent;   // < ukazatel na rodice
-    tASTNode* left;     // < ukazatel na levy podstrom
-    tASTNode* right;    // < ukazatel na pravy podstrom
-    Token token;      // < ukazatel na token
-} tASTNode;
-
-/**
- * @brief struktura pro reprezentaci AST (Abstract Syntax Tree)
- * 
- */
-typedef struct{
-    tASTNode* root; // < ukazatel na korenovy uzel
-    tASTNode* current; // < ukazatel na aktivní uzel
-    tASTNode* last; // < ukazatel na posledni uzel
-    int count; // < pocet uzlu
-} tAST;
-
-Token getNextToken(); // < funkce pro ziskani dalsiho tokenu od LA
+Token getCurrentToken();
+void unexpected_token(Token token);
+void parse();
+int parse_prolog();
+Token code(Token token);
+Token parse_function_definition();
+Token parse_variable_definition();
+Token parse_if();
+Token parse_while();
+Token parse_return();
+Token parse_assignment_or_function_call();
+Token parse_standard_function_call();
+void arguments(Token token);
+void expression();
+void function();
